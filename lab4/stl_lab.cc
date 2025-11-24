8b1a94c1348242426d6a7005edbfacbf0978d47e
Vilgot Jarhede <vilja112@student.liu.se>
2025-11-24 21:57:14 +0100

    Add begin lab4

---
+++ lab4/stl_lab.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct argument {
+    string flag;
+    string parameter;
+};
+
+vector<argument> v = { {"--print", ""},
+                       {"--frequency", ""},
+                       {"--table", ""},
+                       {"--substitute", ""},
+                       {"--remove", ""} };
+
+
+int main(int argc, char *argv[]) {
+
+    // switch (argument) {
+    //     case --print:
+
+    //     case --frequency:
+
+    //     case --table:
+
+    //     case --substitute:
+
+    //     case --remove:
+
+    //     case invaid_argument:
+    // }
+
+}
