b283cc9b634799a9d0ccbde6f2b42f32891378ea
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2025-12-03 23:37:27 +0100

    ...

--- lab3/main.cc
+++ lab3/main.cc
@@ -10,17 +10,6 @@
 
 using namespace std;
 
-/*
-  Ledning och Tips:
-
-  - Modifiera stukturen till en header-fil och en implementationsfil
-  - Utöka 'run()' och 'draw_map()' med övrig funktionalitet.
-  - Lägg alla spöken i en lämplig behållare som en datamedlem.
-  - Bryt ut stora kodblock till egna funktioner.
-  - Använd hjälpfunktioner för att undvika duplicering av kod.
-  - Tänk på att varje funktion inte borde vara längre än 25 rader.
- */
-
 class Ghost_Tester {
   public:
     Ghost_Tester() : pacman{}, ghosts{}, mode{"chase"} {
